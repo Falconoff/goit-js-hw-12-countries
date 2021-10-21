72878e1aac207ee6a442d6a4a369b3399c09688d
Falconoff <sokolovserega1982@gmail.com>
2021-10-22 00:59:55 +0300

    add html-markup

--- src/js/filter.js
+++ src/js/filter.js
@@ -4,25 +4,69 @@ import debounce from 'lodash.debounce';
 // ----------- refs -----------------
 const refs = {
   input: document.getElementById('filter'),
-  message: document.querySelector('.message'),
+  result: document.querySelector('.js-result'),
 };
 
-// import CountriesApi from './fetchCountries';
-// const countriesApiService = new CountriesApi();
+import CountriesApi from './fetchCountries.js';
+const countriesApiService = new CountriesApi();
 
 // refs.input.addEventListener('input', onInput);
 refs.input.addEventListener('input', debounce(onInput, 1000));
 
 function onInput(e) {
+  const find = e.target.value;
   console.log(e.target.value);
   // countriesApiService.fetchCountries(e.target.value);
+
+  fetch(`https://restcountries.com/v2/name/${find}`)
+    .then(r => r.json())
+    .then(r => myF(r))
+    .catch(err => {
+      console.error('Error: ', err);
+    });
 }
 
 // countriesApiService.fetchCountries();
 
 // const url = 'https://restcountries.com/v2/name/ru';
-// fetch('https://restcountries.com/v2/name/ru').then(r => r.json());
-// .then(r => console.log(r));
+
+function myF(arr) {
+  console.log('arr.length=', arr.length);
+  if (arr.length < 1 || arr.length === undefined) console.log('not find ((');
+  if (arr.length === 1) getCountryInfo(arr);
+  if (arr.length >= 2 && arr.length <= 10) getCountriesList(arr);
+  if (arr.length > 10) console.log('Too many matches found. Please enter a more specific query');
+}
+
+// ====== 2 - 10 countries ======
+function getCountriesList(arr) {
+  let listItems = '';
+  arr.forEach(item => {
+    console.log(item.name);
+    listItems += `<li>${item.name}</li>`;
+  });
+  console.log(listItems);
+  refs.result.innerHTML = `<ul>${listItems}</ul>`;
+}
+
+// ====== 1 country ======
+function getCountryInfo(arr) {
+  const item = arr[0];
+  // arr.forEach(item => {
+  //   console.log('==== Country:', item.name.toUpperCase(), '====');
+  //   console.log('capital:', item.capital);
+  //   console.log('population:', item.population);
+  //   item.languages.forEach(language => {
+  //     console.log('lang:', language.name);
+  //   });
+  // });
+  console.log('==== Country:', item.name.toUpperCase(), '====');
+  console.log('capital:', item.capital);
+  console.log('population:', item.population);
+  item.languages.forEach(language => {
+    console.log('lang:', language.name);
+  });
+}
 
 // ========= notify ===============
 import { error } from '@pnotify/core'; // from Creating Notices
@@ -53,4 +97,4 @@ error({
 //   delay: Infinity,
 // });
 
-// refs.message.innerHTML = myAlert;
+// refs.result.innerHTML = myAlert;
